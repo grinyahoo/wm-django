81f37ddf3194208d4aac5ed40cdbdd6e2fdb2fc6
Alexandr Grinevich <devnull@AlexandrsMBP10.fios-router.home>
2019-10-13 22:44:11 -0400

    code cleanup

--- workshop/models.py
+++ workshop/models.py
@@ -60,10 +60,10 @@ class Vehicle(models.Model):
     def __str__(self):
         return "%s %s %s" % (self.year, self.make, self.model)
 
+# TODO implement classes for labor and parts
 class Task(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-# TODO implement classes for labor and parts
     # labor = models.TextField()
     # parts = models.TextField()
     description = models.TextField()
@@ -72,11 +72,9 @@ class Task(models.Model):
     date_filed = models.DateTimeField('date filed')
     date_paid = models.DateTimeField('date paid', default=now_plus_days(360))
     invoiced = models.BooleanField(default=0)
-    # invoice = models.ForeignKey(Invoice, default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=2)
 
     def __str__(self):
-        # return "%s %s %s" % (self.customer, self.vehicle, self.description)
         return "%s %s" % (self.vehicle, self.description)
 
 class Invoice(models.Model):
@@ -86,7 +84,7 @@ class Invoice(models.Model):
     amount_total = models.FloatField(default=0)
     amount_paid = models.FloatField(default=0)
     tasks = models.ManyToManyField(Task)
-    date_paid = models.DateTimeField('date paid', default=now_plus_days(360))
+    date_paid = models.DateTimeField('date paid', null=True)
     paid_in_full = models.BooleanField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
